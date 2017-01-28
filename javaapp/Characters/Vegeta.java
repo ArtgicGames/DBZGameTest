739ab749f733326e940629f31205b89849d70ab2
Artgic <angel.fernandez.artgic@gmail.com>
2017-01-28 18:15:25 -0500

    Removed Comments

--- javaapp/Characters/Vegeta.java
+++ javaapp/Characters/Vegeta.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javaapp.Characters;
 
 import javaapp.Entity;
 
-/**
- *
- * @author angel
- */
 public class Vegeta extends Entity {
 
     public Vegeta() {
